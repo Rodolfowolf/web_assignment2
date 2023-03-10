62568a5c0ae72adc6bfce403537896031eebc8f1
Rodolfo Silva <126601333+Rodolfowolf@users.noreply.github.com>
2023-03-10 15:08:10 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>GOW Ragnarok</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="css/styles.css"/>
+	</head>
+	<body>
+    <main>
+   	<h1>God of War Ragnarok Dev Addresses Puzzles Being Spoiled by Companions</h1>
+      <img src=img/file1.jpg alt="File 1">
+
+    <p><a href="https://www.playstationlifestyle.net/tag/god-of-war-ragnarok">God of War Ragnarok</a> puzzles came with a feature that annoyed quite a few players: <a href="http://www.playstationlifestyle.net/2022/11/26/god-of-war-ragnarok-characters-almost-cut/">companions</a> spoiling the solution mere seconds into solving one. Turns out, this isn’t something play testers picked up on, resulting in the issue making its way to the final game.</p>
+
+      <h2>God of War Ragnarok puzzles being spoiled was an oversight, says dev</h2>
+      <img src=img/logo.png alt="Logo">
+
+    <p>In an interview with <a href="https://www.youtube.com/watch?v=JzevDBqEDCA">MinnMax</a> <a href="https://www.ign.com/articles/god-of-war-ragnarok-devs-didnt-realise-that-allies-spoiling-puzzles-would-annoy-players">via IGN </a>, Santa Monica Studio’s Matt Sophos and Richard Gaubert revealed that allies spoiling puzzles was an oversight that they wish play testers had picked up on. Had it been pointed out to them during development, the developers say they would have fixed it.</p>
+    <p>“It didn’t get exposed to us really until after the game came out, because even when it had play tests we never saw feedback about the characters talking too much or spoiling things too quickly,” Sophos explained. “The timing for the first hint was too aggressive and should have had a much longer countdown timer before something comes up.”</p>
+<p>Santa Monica Studio did hear the criticism from players and took it on board. Sophos and Gaubert didn’t say if they’ll roll out a patch to fix the problem but did promise that it’s something that won’t happen in <a href="http://www.playstationlifestyle.net/2022/11/18/new-god-of-war-game-teased/">the next game</a>.</p>
+    </main>
+      <footer>
+    TAGS: <a href="https://www.playstationlifestyle.net/tag/god-of-war-ragnarok/">GOD OF WAR RAGNAROK</a> | <a href="https://www.playstationlifestyle.net/tag/santa-monica-studio/">SANTA MONICA STUDIO</a> | <a href="https://www.playstationlifestyle.net/tag/sony-interactive-entertainment/">SONY INTERACTIVE ENTERTAINMENT</a> | <a href="https://www.playstationlifestyle.net/2023/02/16/god-of-war-ragnarok-puzzles-spoiled-oversight/"><b>Source</b></a>
+</footer>    
+	</body>
+</html>
